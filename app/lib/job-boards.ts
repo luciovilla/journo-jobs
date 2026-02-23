70357e3c2b566bd2b55f6b57ad8021163b34e5a1
Lucio Villa <lucio310@gmail.com>
2026-02-23 00:20:47 -0500

    more job boards

--- app/lib/job-boards.ts
+++ app/lib/job-boards.ts
@@ -8,6 +8,10 @@ export const JOB_BOARDS = [
     url: "https://job-boards.greenhouse.io/thebaltimorebanner",
   },
   {
+    name: "Bloomberg",
+    url: "https://bloomberg.avature.net/careers/SearchJobs/",
+  },
+  {
     name: "Bloomberg Industry Group",
     url: "https://bloomberg.wd1.myworkdayjobs.com/en-US/Bloombergindustrygroup_External_Career_Site",
   },
@@ -28,8 +32,16 @@ export const JOB_BOARDS = [
     url: "https://condenast.wd5.myworkdayjobs.com/CondeCareers?locationCountry=bc33aa3152ec42d4995f4791a106ed09",
   },
   {
+    name: "Gannett",
+    url: "https://jobs.dayforcehcm.com/en-US/gannett/CANDIDATEPORTAL",
+  },
+  {
     name: "The Guardian",
     url: "https://workwithus.theguardian.com/job-search",
+  },
+  {
+    name: "Hearst",
+    url: "https://eevd.fa.us6.oraclecloud.com/hcmUI/CandidateExperience/en/sites/CX_1/jobs",
   },
   {
     name: "The Marshall Project",
@@ -48,12 +60,24 @@ export const JOB_BOARDS = [
     url: "https://more-perfect-union-action.rippling-ats.com/",
   },
   {
+    name: "New York Post / California Post",
+    url: "https://dowjones.wd1.myworkdayjobs.com/en-US/New_York_Post_Careers",
+  },
+  {
     name: "The New York Times",
     url: "https://www.nytco.com/careers/job-listings/?department=journalism",
   },
   {
+    name: "Pew Research Center",
+    url: "https://pewtrusts.wd5.myworkdayjobs.com/CenterExternal",
+  },
+  {
     name: "Philadelphia Inquirer",
     url: "https://inquirer.rec.pro.ukg.net/PHI1500PHILI/JobBoard/7dc63c3a-f663-4d44-893e-7372f75ba534/?q=&o=postedDateDesc",
+  },
+  {
+    name: "PMC (Penske Media)",
+    url: "https://www.pmc.com/join-our-team/",
   },
   {
     name: "Politico",
@@ -72,15 +96,23 @@ export const JOB_BOARDS = [
     url: "https://simonsfoundation.wd1.myworkdayjobs.com/en-US/simonsfoundationcareers",
   },
   {
+    name: "Thomson Reuters",
+    url: "https://thomsonreuters.wd5.myworkdayjobs.com/en-US/External_Career_Site",
+  },
+  {
     name: "TIME",
     url: "https://recruiting.ultipro.com/TIM1008TIUS/JobBoard/fd1a98f2-b18b-4d7c-98a2-bc7a3a4e9027/",
+  },
+  {
+    name: "Wall Street Journal",
+    url: "https://wsj.jobs/job-category/editorialjournalism/jobs/",
   },
   {
     name: "Warner Bros. Discovery (CNN, Bleacher Report)",
     url: "https://careers.wbd.com/global/en/search-results",
   },
   {
-    name: "Wall Street Journal",
-    url: "https://wsj.jobs/job-category/editorialjournalism/jobs/",
+    name: "Ziff Davis",
+    url: "https://www.ziffdavis.com/careers/jobs",
   },
 ];
